f604ca944bb22f70d2081ea6cad3c1a37816a61a
Thabo Buthelezi <thabo.buthelezi.tb94@gmail.com>
2023-04-12 12:48:34 +0200

    html all forms

---
+++ script.js
@@ -0,0 +1,16 @@
+function getFormValues() {
+
+    let firstName = document.getElementById("fname").value;
+    let lastName = document.getElementById("lname").value;
+    let email = document.getElementById("email").value;
+    let Province = document.getElementById("Province").value;
+
+    console.log(`
+    first name: ${firstName} \n
+    last name: ${lastName} \n
+    email: ${email} \n
+    province: ${Province} \n
+    `)
+
+    
+}
